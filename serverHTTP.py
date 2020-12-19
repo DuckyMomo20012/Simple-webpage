05b22f43417b1d58ef40f1e8cf9ed726a1d6f034
DuckyMomo <64480713+DuckyMomo20012@users.noreply.github.com>
2020-12-19 22:48:55 +0700

    add do_POST func

--- serverHTTP.py
+++ serverHTTP.py
@@ -3,29 +3,30 @@ import os
 import socket
 
 
-class HTTPserver:
-    def __init__(self, HOST, PORT):
-        self.HOST = HOST
-        self.PORT = PORT
+class HttpServer:
+    def __init__(self, host, port):
+        self.HOST = host
+        self.PORT = port
         self.s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind((self.HOST, self.PORT))
         self.s.listen(5)
-        self.request = ""
-        self.request_header = {}
-        self.response = []
+        self.request = str()
+        self.request_header = dict()
+        self.request_body = dict()
+        self.response = list()
 
     def serve_forever(self):
         while True:
-            self.conn, self.addr = self.s.accept()
+            conn, address = self.s.accept()
             try:
-                self.request = self.conn.recv(1024).decode('utf-8')
+                self.request = conn.recv(1024).decode('utf-8')
                 print("Connect to: %s-port: %d" % (self.HOST, self.PORT))
                 self.handle()
-                self.conn.send(b"".join(self.response))
+                conn.send(b"".join(self.response))
                 self.response = []
             finally:
-                self.conn.close()
+                conn.close()
 
     def send_response(self, status_code):
         self.response.append(b"%s %s\r\n" % (b"HTTP/1.1", status_code.encode("utf-8")))
@@ -38,26 +39,34 @@ class HTTPserver:
 
     def translate_path(self):
         self.request = self.request.split("\r\n")
+        parts: str
         for parts in self.request[1:]:
-            line = parts.split(" ")
-            if line[0] != "":
-                self.request_header.update({line[0][:-1]: line[1]})
+            if "&" not in parts:
+                line = parts.split(" ")
+                if line[0] != "":
+                    self.request_header.update({line[0][:-1]: line[1]})
+            else:
+                line = parts.split("&")
+                for pairs in line:
+                    pairs = pairs.split("=")
+                    self.request_body.update({pairs[0]: pairs[1]})
+
         return self.request[0]
 
     def handle(self):
-        requestline = self.translate_path().split(" ")
-        f = str(requestline[1])
+        request_line = self.translate_path().split(" ")
+        f = str(request_line[1])
         if f == '/':
             path = 'index.html'
         else:
             path = f[1:]
-        if requestline[0] == "GET":
+        if request_line[0] == "GET":
             self.do_GET(path)
 
-        if requestline[0] == "POST":
+        if request_line[0] == "POST":
             self.do_POST(path)
 
-        if requestline[0] == "OPTIONS":
+        if request_line[0] == "OPTIONS":
             self.do_OPTION()
 
     def guess_type(self, path):
@@ -85,10 +94,10 @@ class HTTPserver:
     def content_length_send(self, f):
         response = []
         _WINDOWS = os.name == 'nt'
-        COPY_BUFSIZE = 1024 * 1024 if _WINDOWS else 64 * 1024
+        COPY_BUFFSIZE = 1024 * 1024 if _WINDOWS else 64 * 1024
         try:
             while True:
-                buf = f.read(COPY_BUFSIZE)
+                buf = f.read(COPY_BUFFSIZE)
                 if not buf:
                     break
 
@@ -99,33 +108,15 @@ class HTTPserver:
         finally:
             f.close()
 
+    def send_file(self, f, filetype):
+        if filetype == "text/html" or filetype == "text/css":
+            self.content_length_send(f)
+        else:
+            self.chunk_send(f)
+
     def do_GET(self, path):
         f = None
         filetype = self.guess_type(path)
-        if "&" in path:
-            self.do_POST(path)
-            return None
-        try:
-            f = open(path, 'rb')
-            self.send_response("200 OK")
-            self.send_header("Connection", "keep-alive")
-            self.send_header("Content-type", filetype)
-            if filetype == "text/html" or filetype == "text/css":
-                self.content_length_send(f)
-            else:
-                self.chunk_send(f)
-
-        except OSError:
-            f = open('404.html', 'rb')
-            self.send_response('404 NOT FOUND')
-            self.send_header("Connection", "keep-alive")
-            self.send_header("Content-type", filetype)
-            if filetype == "text/html" or filetype == "text/css":
-                self.content_length_send(f)
-            else:
-                self.chunk_send(f)
-
-    def do_POST(self, path):
         if "&" in path:
             query = path.split("&")
             username = query[0].split("=")[1]
@@ -140,6 +131,39 @@ class HTTPserver:
                 path = "404.html"
                 self.send_response("301 MOVED PERMANENTLY")
                 self.send_header("Location", "%s%s" % (url, path))
+            return None
+        try:
+            f = open(path, 'rb')
+            self.send_response("200 OK")
+            self.send_header("Connection", "keep-alive")
+            self.send_header("Content-type", filetype)
+
+        except OSError:
+            f = open('404.html', 'rb')
+            self.send_response('404 NOT FOUND')
+            self.send_header("Connection", "keep-alive")
+            self.send_header("Content-type", filetype)
+        finally:
+            self.send_file(f, filetype)
+
+    def do_POST(self, path):
+        if len(self.request_body) != 0:
+            if "username" and "password" in self.request_body:
+                filetype = self.guess_type(path)
+                username = self.request_body["username"]
+                password = self.request_body["password"]
+                print("username:%s - password:%s" % (username, password))
+                if username == "admin" and password == "admin":
+                    path = "info.html"
+                else:
+                    path = "404.html"
+
+                self.send_response("201 CREATED")
+                self.send_header("Location", "%s%s" % ("/", path))
+                self.send_header("Connection", "keep-alive")
+                self.send_header("Content-type", filetype)
+                f = open(path, 'rb')
+                self.send_file(f, filetype)
 
     def do_OPTION(self):
         self.send_response("204 NO CONTENT")
@@ -148,5 +172,5 @@ class HTTPserver:
         self.send_header("Connection", "keep-alive")
 
 
-server = HTTPserver("127.0.0.1", 80)
+server = HttpServer("127.0.0.1", 80)
 server.serve_forever()
